7d39a822a9cba3446aee4e2eaa66bff278cc456a
Kaanyinaele <110630626+kaanyinaele@users.noreply.github.com>
2024-10-03 15:13:42 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [result, setResult] = useState(null);
+
+  const getInfo = async () => {
+    try {
+      const response = await fetch("/api/whoami"); // Call the API
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json(); // Get the JSON response
+      setResult(data); // Set the result state
+    } catch (error) {
+      console.error("Fetch error: ", error);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        margin: 0,
+        backgroundColor: "rgb(247, 241, 234)",
+      }}
+    >
+      <h1 style={{ fontFamily: '"Chakra Petch", sans-serif', fontWeight: 600 }}>
+        Request Header Parser Microservice
+      </h1>
+      <button
+        onClick={getInfo}
+        style={{
+          padding: "10px 20px",
+          fontSize: "1rem",
+          backgroundColor: "rgb(69, 151, 251)",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          transition: "background-color 0.3s",
+        }}
+      >
+        Get My Info
+      </button>
+      {result && (
+        <pre
+          style={{
+            padding: "20px",
+            borderRadius: "5px",
+            backgroundColor: "rgb(69, 151, 251)",
+          }}
+        >
+          {JSON.stringify(result, null, 2)}
+        </pre>
+      )}
+    </div>
+  );
+}
