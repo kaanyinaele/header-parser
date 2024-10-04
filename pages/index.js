c7a33c8d9bd2a6a7fb88b317e0acb66769a78ee6
kaanyinaele <kaanyinaele@gmail.com>
2024-10-04 01:01:57 +0100

    font fix

--- pages/index.js
+++ pages/index.js
@@ -17,45 +17,55 @@ export default function Home() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        margin: 0,
-        backgroundColor: "rgb(247, 241, 234)",
-      }}
-    >
-      <h1 style={{ fontFamily: '"Chakra Petch", sans-serif', fontWeight: 600 }}>
-        Request Header Parser Microservice
-      </h1>
-      <button
-        onClick={getInfo}
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div
         style={{
-          padding: "10px 20px",
-          fontSize: "1rem",
-          backgroundColor: "rgb(69, 151, 251)",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          transition: "background-color 0.3s",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          margin: 0,
+          backgroundColor: "rgb(247, 241, 234)",
         }}
       >
-        Get My Info
-      </button>
-      {result && (
-        <pre
+        <h1
+          style={{ fontFamily: '"Chakra Petch", sans-serif', fontWeight: 600 }}
+        >
+          Request Header Parser Microservice
+        </h1>
+        <button
+          onClick={getInfo}
           style={{
-            padding: "20px",
-            borderRadius: "5px",
+            padding: "10px 20px",
+            fontSize: "1rem",
             backgroundColor: "rgb(69, 151, 251)",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            transition: "background-color 0.3s",
           }}
         >
-          {JSON.stringify(result, null, 2)}
-        </pre>
-      )}
-    </div>
+          Get My Info
+        </button>
+        {result && (
+          <pre
+            style={{
+              padding: "20px",
+              borderRadius: "5px",
+              backgroundColor: "rgb(69, 151, 251)",
+            }}
+          >
+            {JSON.stringify(result, null, 2)}
+          </pre>
+        )}
+      </div>
+    </>
   );
 }
